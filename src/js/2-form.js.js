8198b6a3b16806943b2c3e068502606ed60799d3
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-05-12 13:28:15 +0300

    3

--- src/js/2-form.js.js
+++ src/js/2-form.js.js
@@ -1,4 +1,24 @@
-const formData = { 
-    email: '',
-    message: ''
+const formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('.message');
+
+form.addEventListener('input', handleInput);
+
+function handleInput(event) {
+//   event.preventDefault();
+  const element = event.target.name;
+    formData[element] = event.target.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
 }
+
+
+function populateInfo (){
+const info = JSON.parse(localStorage.getItem("feedback-form-state"))
+console.log(info)
+}
+
