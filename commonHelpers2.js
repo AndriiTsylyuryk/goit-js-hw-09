1728fdb42ac3df825ba12c6de6dc2ba54d62c358
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-05-12 10:28:32 +0000

    Deploying to gh-pages from @ AndriiTsylyuryk/goit-js-hw-09@8198b6a3b16806943b2c3e068502606ed60799d3 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const t={email:"",message:""},a=document.querySelector(".feedback-form");a.querySelector(".message");a.addEventListener("input",r);function r(e){const o=e.target.name;t[o]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}
+//# sourceMappingURL=commonHelpers2.js.map
